0a754b000c49e0ead8f0ea81cd36ad09bbbe813f
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-08-18 19:52:36 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let theme = localStorage.getItem('theme');
 
 if(theme == null){
-	setTheme('light')
+	setTheme('light');
 }else{
-	setTheme(theme)
+	setTheme(theme);
 };
 
 let themeDots = document.getElementsByClassName('theme-dot');
@@ -11,28 +11,28 @@ let themeDots = document.getElementsByClassName('theme-dot');
 
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
+		let mode = this.dataset.mode;
 		// console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+		setTheme(mode);
+	});
 };
 
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = '../css/style.css'
-	}
+	};
 
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = '../css/blue.css'
-	}
+	};
 
 	if(mode == 'green'){
 		document.getElementById('theme-style').href = '../css/green.css'
-	}
+	};
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = '../css/purple.css'
-	}
+	};
 
-	localStorage.setItem('theme', mode)
+	localStorage.setItem('theme', mode);
 };
